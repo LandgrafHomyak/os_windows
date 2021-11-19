227e276ee16f3f8828499aa03357da8936eb6f3b
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2021-11-20 02:57:56 +0300

    Initial commit

---
+++ _include/COMiC/_os.h
@@ -0,0 +1,8 @@
+//
+// Created by Admin on 20.11.2021.
+//
+
+#ifndef OS_WINDOWS__OS_H
+#define OS_WINDOWS__OS_H
+
+#endif //OS_WINDOWS__OS_H
---
+++ src/info.c
@@ -0,0 +1,4 @@
+//
+// Created by Admin on 20.11.2021.
+//
+
